b9010b6bfcc1c51f70f002a8e9b6306af7a1f1d8
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-03-01 18:41:22 +0100

    Clean up circleci config

--- app/Console/Commands/MakeTestDb.php
+++ app/Console/Commands/MakeTestDb.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Filesystem\Factory;
 use Illuminate\Support\Facades\Storage;
 
 class MakeTestDb extends Command {
@@ -12,14 +11,8 @@ class MakeTestDb extends Command {
 
     protected $description = 'Make test db';
 
-    public function __construct() {
-        parent::__construct();
-    }
-
     public function handle() {
-
         $database = Storage::disk('database');
-        $database->makeDirectory('abc');
         $database->put('testing.sqlite', '');
         $this->call('migrate', [ '--database' => 'sqlite_testing']);
     }
