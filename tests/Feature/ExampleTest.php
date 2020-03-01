8b2b8f13db930661835352e3c6987e5892e42ba8
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-03-01 14:38:43 +0100

    Format test code

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+class ExampleTest extends TestCase {
 
+    public function testBasicTest() {
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
+
 }
