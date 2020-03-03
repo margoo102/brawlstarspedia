d2cd4147042d5079930c7213367a6813b53082ef
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-03-03 18:53:23 +0100

    Add basic routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us', function() {
+   return 'About us page';
+});
+
+Route::get('/categories/{url?}',function($url = null){
+    return $url == null ? 'list all categories' : "display $url category";
+})->name('categories');
+
+Route::get('/blawler/name', 'ShowBrawlerController');
