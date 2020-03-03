e2826582a92a098f40b5707e6a219749c5026553
Krzysztof Maziarz <krzysztof.maziarz@gmail.com>
2020-03-03 19:30:37 +0100

    Add brawlers controller

--- app/Http/Controllers/API/BrawlerController.php
+++ app/Http/Controllers/API/BrawlerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class BrandController extends Controller
+class BrawlerController extends Controller
 {
     /**
      * Display a listing of the resource.
