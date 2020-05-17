e8147e3a6c4694a25987ea8cd095a3980bce502c
Małgorzata Maziarz <malgorzata.maziarz@gmail.com>
2020-05-17 18:08:38 +0200

    Remove unused resources

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,6 @@
 
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
 
         <link href="{{ asset('/css/app.css') }}" />
 
